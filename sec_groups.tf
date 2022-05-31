1a0d53016630bfbe4f2ff9eb78fd6260dc370ee8
laslopaul <96680549+laslopaul@users.noreply.github.com>
2022-05-31 14:27:02 +0300

    create sec_groups.tf

---
+++ sec_groups.tf
@@ -0,0 +1,20 @@
+# Create security group for WinVM, making it accessible via RDP
+resource "azurerm_network_security_group" "nsg_winvm" {
+  name                = "NSG_WinVM"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "AllowRDPInbound"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3389"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+}
+
